7427764cdd1b56c1ca06b91108202106b4f1cb86
sabin karki <nibaserc@gmail.com>
2019-08-18 15:27:14 -0400

    Added send email functionality and test case

--- src/main/java/com/learn/email/service/EmailServiceImpl.java
+++ src/main/java/com/learn/email/service/EmailServiceImpl.java
@@ -6,10 +6,10 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.*;
 
 @Service
 public class EmailServiceImpl implements EmailService {
@@ -26,12 +26,60 @@ public class EmailServiceImpl implements EmailService {
     @Override
     public Boolean sendEmail(Email email) {
         boolean result = false;
+        String crlfPattern = "\\r\\n|\\n";
 
         if (prodMode.equalsIgnoreCase("false")) {
             email.setTo(getAllowedAddress(email.getTo()));
             email.setCc(getAllowedAddress(email.getCc()));
         }
+        Properties prop = new Properties();
+        prop.put("mail.smtp.host", mapProperties.get("host"));
+        prop.put("mail.smtp.port", mapProperties.get("port"));
+        prop.put("mail.smtp.auth", mapProperties.get("authrname"));
+        prop.put("mail.smtp.starttls.enable", mapProperties.get("starttls.enable"));
 
+
+        //  properties.put("mail.smtp.port", "name of email server");
+        //Session session = Session.getInstance(prop);
+        Session session = Session.getInstance(prop,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(mapProperties.get("username"), mapProperties.get("password"));
+                    }
+                });
+
+        try {
+            InternetAddress[] addresses = new InternetAddress[email.getTo().length];
+            for (int i = 0; i < addresses.length; i++) {
+                addresses[i] = new InternetAddress(email.getTo()[i]);
+            }
+            Message message = new MimeMessage(session);
+            message.addHeader("Content-type", "text/HTML; charset=UTF-8");
+            String subject = email.getSubject().replace(crlfPattern, " ");
+            message.setSubject(subject);
+
+            String content = email.getContent().replace(crlfPattern, System.lineSeparator());
+            message.setContent(content, email.getMimeType());
+            message.setRecipients(Message.RecipientType.TO, addresses);
+            message.setSentDate(new Date());
+
+            if (StringUtils.isNotEmpty(email.getFrom())) {
+                InternetAddress[] fromAddress = new InternetAddress[1];
+                fromAddress[0] = new InternetAddress(email.getFrom());
+                message.addFrom(fromAddress);
+            }
+            if (ArrayUtils.isNotEmpty(email.getCc())) {
+                InternetAddress[] ccAddresses = new InternetAddress[email.getCc().length];
+                for (int i = 0; i < addresses.length; i++) {
+                    ccAddresses[i] = new InternetAddress(email.getCc()[i]);
+                    message.setRecipients(Message.RecipientType.CC, ccAddresses);
+                }
+            }
+            Transport.send(message);
+            result = true;
+        } catch (MessagingException e) {
+            System.out.println(e.getMessage());
+        }
         return result;
     }
 
--- src/test/java/com/learn/email/service/EmailServiceTest.java
+++ src/test/java/com/learn/email/service/EmailServiceTest.java
@@ -1,9 +1,11 @@
 package com.learn.email.service;
 
+import com.learn.email.model.Email;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.util.ReflectionTestUtils;
 
@@ -23,8 +25,12 @@ public class EmailServiceTest {
     @Before
     public void setUp() {
         Map<String, String> mapProperties = new HashMap();
-        mapProperties.put("host", "mail.smtp.host");
-        mapProperties.put("server", "test");
+        mapProperties.put("host", "smtp.gmail.com");
+        mapProperties.put("port", "587");
+        mapProperties.put("authrname", "true");
+        mapProperties.put("starttls.enable", "true");
+        mapProperties.put("username", "****");
+        mapProperties.put("password", "****");
 
         ReflectionTestUtils.setField(emailServiceImpl, "prodMode", "false");
         ReflectionTestUtils.setField(emailServiceImpl, "mapProperties", mapProperties);
@@ -40,8 +46,31 @@ public class EmailServiceTest {
 
     @Test
     public void getAllowedAddressForGivenRequest() {
-        String[] request = {"test@gmail.com","test@yahoo.com","test@hotmail.com","test@test.com"};
+        String[] request = {"test@gmail.com", "test@yahoo.com", "test@hotmail.com", "test@test.com"};
         String[] acutalResults = this.emailServiceImpl.getAllowedAddress(request);
         assertEquals(3, acutalResults.length);
+    }
+
+    @Test
+    public void sendEmail() {
+        Email email = new Email();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<html>");
+        sb.append("<head><title></title></head>");
+        sb.append("<body>");
+        sb.append("<b>Dear " + "Nikita" + "<b><br><br><br>");
+        sb.append("This email testing is for <b>Nikita<b> from <b>Sabin<b><br><br><br>");
+        sb.append("Thank you, <br><br>");
+        sb.append("Love You");
+        sb.append("</body></html>");
+
+        email.setTo(new String[]{"****", ""});
+        email.setSubject("I Love You");
+        email.setMimeType(MediaType.TEXT_HTML_VALUE);
+        email.setContent(sb.toString());
+
+        this.emailServiceImpl.sendEmail(email);
+
     }
 }
